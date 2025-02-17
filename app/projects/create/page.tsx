2a097615558d5bd6801a7010e9a4118aadd97865
Tiago Bankow <tiago.bankow@gmail.com>
2025-02-16 23:33:55 -0300

    refactor: clear project files

--- app/projects/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProjectForm } from "./_components/create-project-form";
-
-export default function CreateProject() {
-  return (
-    <div className="p-4">
-      <ProjectForm />
-    </div>
-  );
-}
