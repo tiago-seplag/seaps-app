5db09cac200d97ec7644ce62b26f80fd8677f888
Tiago Bankow <tiago.bankow@gmail.com>
2025-02-25 14:12:59 -0400

    feat: charts & mockup dashboard

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -1,3 +1,15 @@
+import { AreaComponent } from "./(dashboard)/area-card";
+import { BarComponent } from "./(dashboard)/bar-card";
+import { HorizontalBarComponent } from "./(dashboard)/horizontal-bar-card";
+import { PieComponent } from "./(dashboard)/pie-card";
+
 export default async function Page() {
-  return <div className="flex flex-col">Home Route</div>;
+  return (
+    <div className="grid grid-cols-3">
+      <BarComponent />
+      <PieComponent />
+      <HorizontalBarComponent />
+      <AreaComponent />
+    </div>
+  );
 }
