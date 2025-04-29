e0d94497ad6560e8e9b3dcedae538b63138300b2
Tiago Bankow <tiago.bankow@gmail.com>
2025-04-29 16:59:39 -0400

    fix: default open sidebar

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -7,16 +7,20 @@ import {
 } from "@/components/ui/sidebar";
 import { ModeToggle } from "@/components/mode-toggle";
 import { UserProvider } from "@/contexts/user-context";
+import { cookies } from "next/headers";
 
-export default function Layout({
+export default async function Layout({
   children,
   breadcrumb,
 }: Readonly<{
   children: React.ReactNode;
   breadcrumb?: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={defaultOpen}>
       <AppSidebar />
       <SidebarInset className="gap-2 py-2">
         <header className="mx-2 flex h-16 items-center justify-between gap-2 rounded border bg-card px-4 shadow">
