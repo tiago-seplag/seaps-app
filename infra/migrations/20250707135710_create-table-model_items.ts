a07df7483affb48bdc236dc258f84500aaeb0db6
tiago-seplag <tiagobankow@seplag.mt.gov.br>
2025-07-07 14:10:01 +0000

    feat: create migration files for models and model_items tables with necessary constraints and relationships

---
+++ infra/migrations/20250707135710_create-table-model_items.ts
@@ -0,0 +1,22 @@
+import type { Knex } from "knex";
+
+const TABLE_NAME = "model_items";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(TABLE_NAME, (table) => {
+    table.uuid("id", { primaryKey: true }).defaultTo(knex.fn.uuid());
+    table.uuid("model_id").notNullable();
+    table.uuid("item_id").notNullable();
+    table.string("name").notNullable();
+    table.boolean("is_deleted").defaultTo(false);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+
+    table.foreign("model_id").references("id").inTable("models");
+    table.foreign("item_id").references("id").inTable("items");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable(TABLE_NAME);
+}
