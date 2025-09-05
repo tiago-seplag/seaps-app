005c35c67037ef70ede36b9b8f471057f15fc432
Tiago Bankow <tiago.bankow@gmail.com>
2025-09-05 15:29:11 -0400

    feat: enhance PDF generation handler with error handling and checklist item retrieval

--- app/api/v1/reports/[id]/route.ts
+++ app/api/v1/reports/[id]/route.ts
@@ -1,46 +1,50 @@
-import { ValidationError } from "@/errors/validation-error";
-import { getChecklistById } from "@/models/checklist";
 import { NextRequest } from "next/server";
-import axios from "axios";
 import controller, { handler } from "@/infra/controller";
+import checklist from "@/models/checklist";
+import axios from "axios";
+import checklistItem from "@/models/checklist-item";
 
 async function getHandler(
   _: NextRequest,
   { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
-  try {
-    const checklist = await getChecklistById(id);
+  const _checklist = await checklist.getChecklistById(id);
 
-    const response = await axios.post(
-      process.env.REPORT_URL + "?id=" + id,
-      checklist,
-      { responseType: "arraybuffer" },
-    );
+  const checklistItems = await checklist.getChecklistItems(id);
 
-    if (response.status !== 200) {
-      return Response.json(
-        { error: "Erro ao gerar o PDF" },
-        { status: response.status },
-      );
-    }
+  const items = [];
 
-    const body = Buffer.from(response.data, "binary");
+  for (const _checklistItem of checklistItems) {
+    const item = await checklistItem.findById(_checklistItem.id);
 
-    return new Response(body, {
-      status: 200,
-      headers: {
-        "Content-Type": "application/pdf",
-        "Content-Length": String(body.byteLength),
-        "Cache-Control": "public, max-age=300",
-        "Content-Disposition": "inline; filename=generated.pdf",
-      },
-    });
-  } catch (error) {
-    if (error instanceof ValidationError)
-      return Response.json(error, { status: error.statusCode });
-    return Response.json({ error }, { status: 500 });
+    items.push(item);
   }
+
+  const response = await axios.post(
+    process.env.REPORT_URL + "?id=" + id,
+    { ..._checklist, items },
+    { responseType: "arraybuffer" },
+  );
+
+  if (response.status !== 200) {
+    return Response.json(
+      { error: "Erro ao gerar o PDF" },
+      { status: response.status },
+    );
+  }
+
+  const body = Buffer.from(response.data, "binary");
+
+  return new Response(body, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/pdf",
+      "Content-Length": String(body.byteLength),
+      "Cache-Control": "public, max-age=300",
+      "Content-Disposition": "inline; filename=generated.pdf",
+    },
+  });
 }
 
 export const GET = handler([controller.authenticate], getHandler);
